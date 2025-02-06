a2949137514d263627f82a6c294f381539219610
jkfletcher98 <jkfletcher@bsu.edu>
2025-02-06 18:13:22 +0000

    Started Horse object

--- horseRace-oop.cpp
+++ horseRace-oop.cpp
@@ -1,3 +1,45 @@
 #include <iostream>
 #include <random>
-std::random
+std::random_device rd;
+std::uniform_int_ditribution<int> dist(0,1);
+
+class Horse {
+	private:
+		int position;
+		int id;
+		int trackLength;
+
+	public:
+		Horse(){
+		} // end constructor
+
+		void init(int id, int trackLength){
+			Horse::id = id;
+			Horse::trackLength = trackLength;
+		} // end init
+		
+		void advance(){
+			int coin = dist(rd);
+			position += coin;
+		} // end advance
+
+		void printLane(){
+			for (int space = 0; space < trackLength; space++){
+				if (space == position){
+					printf("%d \n", id)
+				}
+				else {
+					printf(". ")
+				}
+			} // end for
+		} // end printLane
+
+		bool isWinner(){
+			bool result = false;
+			if (position > trackLength){
+				result = true
+			} // end if
+
+			return result;
+		} // end isWinner
+}; // end Horse
