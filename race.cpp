d81f4c67c48c698bde1f886a29f0651df79467ac
jkfletcher98 <jkfletcher@bsu.edu>
2025-02-07 19:27:13 +0000

    Created constructor

--- race.cpp
+++ race.cpp
@@ -2,4 +2,10 @@
 #include "race.h"
 
 Race::Race(){
+	const int TRACK_LENGTH = 15;
+	const int NUM_HORSES = 5;
+	Horse::horses[NUM_HORSES] = {0, 0, 0, 0, 0}
+}; // end constructor
+
+void Race::run(){
 
