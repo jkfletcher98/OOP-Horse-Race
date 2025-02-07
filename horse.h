919c3305e896966a3e199bd3b54a29d0fdb4182a
jkfletcher98 <jkfletcher@bsu.edu>
2025-02-07 14:14:06 +0000

    Created horse.h

---
+++ horse.h
@@ -0,0 +1,16 @@
+#ifndef HORSE_H_EXISTS
+#define HORSE_H_EXISTS
+
+class Horse {
+	private:
+		int position;
+		int trackLength;
+		int id;
+
+	public:
+		Horse();
+		init (int id, int trackLength);
+		void advance();
+		void printLane();
+		bool isWinner();
+	}; // end Horse
