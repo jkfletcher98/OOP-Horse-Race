783581614bcdc75a59de1e9cd733f98c87894cb1
jkfletcher98 <jkfletcher@bsu.edu>
2025-02-07 14:52:00 +0000

    Added horse.cpp

---
+++ horse.cpp
@@ -0,0 +1,38 @@
+#include "horse.h"
+
+Horse::Horse(){
+	position = 0;
+	id = 0;
+	trackLength = 15;
+}
+
+Horse::void init(int id, int trackLength){
+	Horse::id = id;
+	Horse::trackLength = trackLength;
+} // end init
+
+
+Horse::void advance(){
+	int coin = dist(rd);
+	Horse::position += coin;
+} // end advance
+
+Horse::void printLane(){
+	for (int space = 0; space < tackLength; space++){
+		if (space == position){
+			printf("%d, \n ", id);
+		}
+		else {
+			printf(". ");
+		}
+	}
+} // end printLane
+
+Horse::bool isWinner(){
+	bool result = false;
+	if (position > trackLength){
+		result = true;
+		printf("Horse %d wins! \n", id);
+	}
+}
+
